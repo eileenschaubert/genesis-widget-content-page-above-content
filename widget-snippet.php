9b5fc275f81eb7c88ce6e11b44851d5feafb58d0
eileenschaubert <eileen.schaubert@gmail.com>
2020-10-05 12:39:22 -0500

    Update to code used in Genesis / Mai theme
    Updated snippet with Mai theme (classic) specific info

--- widget-snippet.php
+++ widget-snippet.php
@@ -1,16 +1,21 @@
 <?php
 
-/** Register widget areas */
-	//Widget Area Above Content Area Excludes the Front Page
+// Code snippet but with Mai theme specifics
+// Run after Mai Theme.
+//This is the trick for Mai Themes - must be done after theme setup
+add_action( 'after_setup_theme', function() {
+
+	/** Register widget areas */
+	//Widget Area to Create Custom Navigation Above Content Area
 	genesis_register_sidebar( array(
 	  'id' => 'content-page-above-content',
-	  'name' => __( 'Content Page Above Content', 'es' ),
-	  'description' => __( 'This is for content navigation', 'es' ),
+	  'name' => __( 'Content Page Above Content', 'jr' ),
+	  'description' => __( 'This is for content navigation', 'jr' ),
 	) );
 
-	add_action ('genesis_after_header','es_content_page_above_content', 5);
+	add_action ('genesis_after_header','jr_content_page_above_content', 5);
 
-	function es_content_page_above_content() {
+	function jr_content_page_above_content() {
 		if (!is_front_page()){
 	 		genesis_widget_area( 'content-page-above-content', array(
 	    	  'before' => '<div id="content-page-above-content-container" class="content-page-above-content-container menu">',
@@ -18,3 +23,5 @@
 			));
 	}
 	}
+
+});
